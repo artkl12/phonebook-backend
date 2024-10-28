66c113b8f55c82d497e1e0fccb4daed606736340
artkl12 <arta.klauze@gmail.com>
2024-10-28 12:59:40 +0200

    commit

--- models/person.js
+++ models/person.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
-
 mongoose.set("strictQuery", false);
-
 const url = process.env.MONGODB_URI;
-
 console.log("connecting to", process.env.MONGODB_URI);
 
 mongoose
